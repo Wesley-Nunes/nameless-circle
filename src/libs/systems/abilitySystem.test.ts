5c4df41d924c49788f43e8ff3619b29b8259cdb3
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-07-28 12:41:12 -0300

    test: abilitySystem throws an error when the template is invalid

--- src/libs/systems/abilitySystem.test.ts
+++ src/libs/systems/abilitySystem.test.ts
@@ -1,7 +1,38 @@
 import { describe, it, expect } from 'vitest'
+import getEnemyAbilities from './abilitySystem'
 
 describe('abilitySystem', () => {
-    it('', () => {
+    it('should throw an error for an invalid template', () => {
+        // @ts-expect-error  
+        expect(() => getEnemyAbilities({}, 1)).toThrowError(
+            `Invalid template: '{}'.
+                        - Expected keys: str, dex, con, int, wis, cha
+                        - Expected values: Non-negative number`
+        )
+
+        // @ts-expect-error  
+        expect(() => getEnemyAbilities({ a: 2, b: 3 }, 1)).toThrowError(
+            `Invalid template: '{"a":2,"b":3}'.
+                        - Expected keys: str, dex, con, int, wis, cha
+                        - Expected values: Non-negative number`
+        )
+
+        expect(() => getEnemyAbilities(
+            // @ts-expect-error  
+            { str: 'a', dex: 'b', con: 'c', int: 'd', wis: 'e', cha: 'f' }, 1)
+        ).toThrowError(
+            `Invalid template: '{"str":"a","dex":"b","con":"c","int":"d","wis":"e","cha":"f"}'.
+                        - Expected keys: str, dex, con, int, wis, cha
+                        - Expected values: Non-negative number`
+        )
+
+        expect(() => getEnemyAbilities(
+            { str: -2, dex: 1, con: 2, int: 3, wis: 1, cha: 2 }, 1)
+        ).toThrowError(
+            `Invalid template: '{"str":-2,"dex":1,"con":2,"int":3,"wis":1,"cha":2}'.
+                        - Expected keys: str, dex, con, int, wis, cha
+                        - Expected values: Non-negative number`
+        )
     })
 })
 
--- src/libs/systems/abilitySystem.ts
+++ src/libs/systems/abilitySystem.ts
@@ -2,7 +2,22 @@ type AbilityKey = 'str' | 'dex' | 'con' | 'int' | 'wis' | 'cha'
 type AbilityTemplate = Record<AbilityKey, number>
 type AbilityCharacter = Record<AbilityKey, { score: number, modifier: number }>
 
-const getEnemyAbilities = (template: AbilityTemplate, xp: number): AbilityCharacter => { }
+const validateTemplate = (template: AbilityTemplate) => {
+    const keys: AbilityKey[] = ['str', 'dex', 'con', 'int', 'wis', 'cha']
+    const isEmpty = !template || !Object.values(template).length
+    const invalidKeys = !Object.keys(template).every((k) => keys.includes(k as AbilityKey))
+    const invalidValues = !Object.values(template).every((v) => typeof v === 'number' && v > 0)
+
+    const isInvalid = isEmpty || invalidKeys || invalidValues
+    if (isInvalid) {
+        throw new Error(`Invalid template: '${JSON.stringify(template)}'.
+                        - Expected keys: str, dex, con, int, wis, cha
+                        - Expected values: Non-negative number`)
+    }
+}
+const getEnemyAbilities = (template: AbilityTemplate, xp: number): AbilityCharacter => {
+    validateTemplate(template)
+}
 
 export default getEnemyAbilities
 
