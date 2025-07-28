16a08c5f30901f6e296ea49f71d22a6b36ba0e13
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-07-28 19:19:47 -0300

    feat: Implement enemy ability block generation from template

--- src/libs/systems/abilitySystem/abilitySystem.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import getEnemyAbilities from './abilitySystem'
-
-describe('abilitySystem', () => {
-    it.todo('should return a high-dexterity distribution for a sharpshooter template')
-})
-
