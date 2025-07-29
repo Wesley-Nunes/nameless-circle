f803cdfaac80fbaa8e1107730190adcc99415563
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-07-28 21:26:46 -0300

    feat: implement unique ID generation for entities

--- src/libs/systems/validationSystem/index.ts
+++ src/libs/systems/validationSystem/index.ts
@@ -1,4 +1,5 @@
+export { default as validateEnemyXp } from './validateEnemyXp'
+export { default as validateIDParam } from './validateIDParam'
 export { default as validatePositiveNumber } from './validatePositiveNumber'
 export { default as validateTemplate } from './validateTemplate'
-export { default as validateEnemyXp } from './validateEnemyXp'
 
