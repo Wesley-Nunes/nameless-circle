212407b212e1542a7f5b7e57b67056345273e410
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-08-03 14:09:33 -0300

    feat: Add minimum party size validation

--- src/libs/systems/validationSystem/index.ts
+++ src/libs/systems/validationSystem/index.ts
@@ -2,6 +2,7 @@ export { default as validateCharacterSize } from './validateCharacterSize'
 export { default as validateEnemyXp } from './validateEnemyXp'
 export { default as validateSpecies } from './validateSpecies'
 export { default as validateIntegerNumber } from './validateIntegerNumber'
+export { default as validatePartySize } from './validatePartySize'
 export { default as validatePositiveNumber } from './validatePositiveNumber'
 export { default as validateTemplate } from './validateTemplate'
 
