f2504d2af8df289e503a76af2a7ce9ac6d41664a
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-08-04 13:26:46 -0300

    feat: Add weapon modifier calculator utility

---
+++ src/libs/systems/abilitySystem/getWeaponModifier.ts
@@ -0,0 +1,14 @@
+import type { AbilityBlock, Weapon } from 'libs/entities'
+
+const getWeaponModifier = (weapon: Weapon, characterAbilities: AbilityBlock): number => {
+    if (weapon.range === 'melee') {
+        return characterAbilities.str.modifier
+    } else if (weapon.range === 'ranged') {
+        return characterAbilities.dex.modifier
+    }
+
+    return 0
+}
+
+export default getWeaponModifier
+
