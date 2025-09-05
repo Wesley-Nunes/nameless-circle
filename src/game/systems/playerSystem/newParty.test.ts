3ecd7b56b7837ed98df991aa6fcc7414459b2170
Wesley-Nunes <wesnmonteiro@gmail.com>
2025-09-05 14:17:10 -0300

    test: Add unit tests for newParty behavior

--- src/game/systems/playerSystem/newParty.test.ts
+++ src/game/systems/playerSystem/newParty.test.ts
@@ -2,14 +2,75 @@ import { describe, expect, it } from 'vitest'
 
 import newParty from './newParty'
 
-import type { Hero } from 'game/types'
-import { addID } from '../IDSystem'
+import { getHeroById } from 'game/data/accessors'
 
 describe('newParty', () => {
-    it.todo('successfully adds valid hero to party', () => {})
-    it.todo('throws when old party size is invalid', () => {})
-    it.todo('throws when new hero ID is invalid', () => {})
-    it.todo('throws when new party size becomes invalid', () => {})
-    it.todo('does not mutate original party', () => {})
-    it.todo('handles adding to empty party', () => {})
+    it('successfully adds valid hero to party', () => {
+        const oldParty = [getHeroById('hero_0001')]
+        const availableHeroIds = ['hero_0001', 'hero_0002']
+        const newHeroId = 'hero_0002'
+        const expectedParty = [...oldParty, getHeroById('hero_0002')]
+
+        const newHeroParty = newParty(oldParty, availableHeroIds, newHeroId)
+
+        expect(newHeroParty).toStrictEqual(expectedParty)
+    })
+    it('throws when old party size is invalid', () => {
+        expect(() => {
+            const oldParty = [
+                getHeroById('hero_0001'),
+                getHeroById('hero_0002'),
+                getHeroById('hero_0003'),
+                getHeroById('hero_0004')
+            ]
+            const availableHeroIds = [
+                'hero_0001',
+                'hero_0002',
+                'hero_0003',
+                'hero_0004',
+                'hero_0005'
+            ]
+            const newHeroId = 'hero_0005'
+
+            newParty(oldParty, availableHeroIds, newHeroId)
+        }).toThrowError(
+            'Maximum party size reached (3 heroes). ' +
+                'Please remove a hero first using removeFromParty().'
+        )
+    })
+    it('throws when new hero ID is invalid', () => {
+        expect(() => {
+            const oldParty = [
+                getHeroById('hero_0001'),
+                getHeroById('hero_0002')
+            ]
+            const availableHeroIds = ['hero_0001', 'hero_0002', 'hero_0003']
+            const newHeroId = 'hero_0004'
+
+            newParty(oldParty, availableHeroIds, newHeroId)
+        }).toThrowError(
+            'Invalid (ID: hero_0004). Available IDs: hero_0001, hero_0002, hero_0003'
+        )
+    })
+    it('throws when new party size becomes invalid', () => {
+        expect(() => {
+            const oldParty = [
+                getHeroById('hero_0001'),
+                getHeroById('hero_0002'),
+                getHeroById('hero_0003')
+            ]
+            const availableHeroIds = [
+                'hero_0001',
+                'hero_0002',
+                'hero_0003',
+                'hero_0004'
+            ]
+            const newHeroId = 'hero_0004'
+
+            newParty(oldParty, availableHeroIds, newHeroId)
+        }).toThrowError(
+            'Maximum party size reached (3 heroes). ' +
+                'Please remove a hero first using removeFromParty().'
+        )
+    })
 })
